ae2b2f382c283e488617bfd1ac45e993fa6aea83
k1r03478 <dipaksharma34447255@gmail.com>
2023-08-30 20:07:53 +0545

    Day-2 learning of Rust language

---
+++ day-2/target/debug/day-2.d
@@ -0,0 +1,1 @@
+E:\Projects\Blockchain\RUST\day-2\target\debug\day-2.exe: E:\Projects\Blockchain\RUST\day-2\src\main.rs
---
+++ day-2/target/debug/deps/day_2.d
@@ -0,0 +1,5 @@
+E:\Projects\Blockchain\RUST\day-2\target\debug\deps\day_2.exe: src\main.rs
+
+E:\Projects\Blockchain\RUST\day-2\target\debug\deps\day_2.d: src\main.rs
+
+src\main.rs:
